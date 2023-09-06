2a568b5fd1b5c7ab33cb017f8e7b1e43d37af698
Harshikesh <harshibabu604@gmail.com>
2023-09-06 23:05:25 +0530

    code added to test complete reservation page.

--- flightReservation/src/main/java/com/harshi/flightReservation/controllers/ReservationController.java
+++ flightReservation/src/main/java/com/harshi/flightReservation/controllers/ReservationController.java
@@ -3,10 +3,12 @@ package com.harshi.flightReservation.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.harshi.flightReservation.entities.Flight;
+import com.harshi.flightReservation.entities.Passenger;
 import com.harshi.flightReservation.repos.FlightRepository;
 @Controller
 public class ReservationController {
@@ -20,5 +22,14 @@ public class ReservationController {
 		Flight flight = flightRepository.findById(flightId).get();
 		modelMap.addAttribute("flight", flight);
 		return "login/completeReservation";}
+	
+	
+	@RequestMapping(value="/completeReservation")
+	public String completeReservation(@ModelAttribute Passenger passenger,Long flightId) {
+		String name=passenger.getFirstName()+" "+passenger.getLastName();
+		String email = passenger.getEmail();
+		return "login/showReservation";
+		
+	}
 
 }
--- flightReservation/src/main/java/com/harshi/flightReservation/entities/Reservation.java
+++ flightReservation/src/main/java/com/harshi/flightReservation/entities/Reservation.java
@@ -8,6 +8,30 @@ import lombok.Data;
 @Data
 public class Reservation extends AbstractEntity {
 	private Boolean checkedIn;
+	public Boolean getCheckedIn() {
+		return checkedIn;
+	}
+	public void setCheckedIn(Boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+	public int getNumOfBags() {
+		return numOfBags;
+	}
+	public void setNumOfBags(int numOfBags) {
+		this.numOfBags = numOfBags;
+	}
+	public Passenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
 	private int numOfBags;
 	@OneToOne
 	private Passenger passenger;
